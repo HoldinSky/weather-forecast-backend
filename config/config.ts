6cf00a93fde113f502d64c3ccf3efef31aa9bb89
nazar <nkrylov.biz@gmail.com>
2024-01-28 10:57:23 +0200

    project setup: postgres connection

---
+++ config/config.ts
@@ -0,0 +1,29 @@
+import * as dotenv from "dotenv";
+import ConfigDTO from "./config.dto";
+
+dotenv.config({ path: "./.env" });
+
+const getConfig: () => ConfigDTO = () => {
+  console.log(process.env.POSTGRES_URL);
+
+  return {
+    node: {
+      port: process.env.PORT,
+      pythonServiceUrl: process.env.PYTHON_SERVICE_URL,
+    },
+    postgres: {
+      dialect: "postgres",
+      uri: process.env.POSTGRES_URL,
+      define: {
+        timestamps: false,
+      },
+      logging: false,
+    },
+    mongo: {
+      uri: process.env.MONGO_URL,
+      options: {},
+    },
+  };
+};
+
+export default getConfig;
