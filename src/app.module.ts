a9362c0c4bbf1cb2c540783952f061650eaf8ea8
nazar <nkrylov.biz@gmail.com>
2024-01-28 10:57:23 +0200

    project setup: postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import config from "config/config";
+import { SequelizeModule, SequelizeModuleOptions } from "@nestjs/sequelize";
+import { ConfigModule } from "@nestjs/config";
+import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
+import { AllExceptionsFilter } from "./utils/filters/all-exceptions.filter";
+import { LoggingInterceptor } from "./utils/interceptors/logging.interceptor";
+import { WinstonModule } from "nest-winston";
+import { winstonConfig } from "./services/winston/winston";
+
+const defaultConfig: SequelizeModuleOptions = {
+  ...config().postgres,
+  synchronize: true,
+  autoLoadModels: true,
+  models: [],
+};
+
+// const mongoUri: string = config().mongo.uri;
+// const mongoOptions: MongooseModuleOptions = config().mongo.options;
 
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot(winstonConfig()),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
+    }),
+    SequelizeModule.forRoot(defaultConfig),
+    // MongooseModule.forRoot(mongoUri, mongoOptions),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
