031f51a85582c77779915487a02532404b8bb1d2
nazar <nkrylov.biz@gmail.com>
2024-02-02 20:29:38 +0200

    fix commit

--- src/modules/startup/startup-actions.service.spec.ts
+++ src/modules/startup/startup-actions.service.spec.ts
@@ -1,11 +1,4 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { AppController } from "../../app.controller";
-import { AppService } from "../../app.service";
 import { StartupActions } from "./startup-actions.service";
-import { HourlyForecast } from "../database/hourly/hourly.model";
-import { DailyForecast } from "../database/daily/daily.model";
-import { LocationService } from "../location/location.service";
-import { PythonService } from "../python/python.service";
 
 describe("StartupActionsService", () => {
   let startupActions: StartupActions
