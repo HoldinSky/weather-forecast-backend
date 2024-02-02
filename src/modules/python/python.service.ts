a1a4c068bf6184757f5600f8112acdd3f6534d40
nazar <nkrylov.biz@gmail.com>
2024-02-02 21:33:43 +0200

    reducing requests to python

--- src/modules/python/python.service.ts
+++ src/modules/python/python.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import axiosRequest from "../../services/apiClient/apiClient";
 import { RequestMethods } from "../../services/apiClient/apiClient.dto";
-import { Coordinates, MILLIS_IN_DAY } from "../../utils/helper";
+import { Coordinates } from "../../utils/helper";
 import { PythonResponse } from "../API/models";
 
 @Injectable()
@@ -10,24 +10,22 @@ export class PythonService {
   private readonly defaultPredictDays = 8;
 
   async fetchPredictForDays(coords: Coordinates, count: number = this.defaultPredictDays) {
-    const days: string[] = [];
-
-    for (let i = 0; i <= count; i++) {
-      const dateString = new Date(Date.now() + i * MILLIS_IN_DAY).toISOString();
-      days.push(dateString.substring(0, dateString.indexOf("T")));
-    }
+    const dateString = new Date(Date.now()).toISOString();
 
     const response: PythonResponse[][] = [];
-    for (const day of days) {
-      const resp = await axiosRequest<undefined, { start_date: string }, PythonResponse[]>({
-        url: this.predictUrl,
-        method: RequestMethods.GET,
-        params: { start_date: day, latitude: coords.lat, longitude: coords.lon }
-      });
+    const resp = await axiosRequest<undefined, { start_date: string }, PythonResponse[]>({
+      url: this.predictUrl,
+      method: RequestMethods.GET,
+      params: {
+        start_date: dateString.substring(0, dateString.indexOf("T")),
+        latitude: coords.lat,
+        longitude: coords.lon,
+        periods: count * 24
+      }
+    });
 
-      if (resp.response)
-        response.push(resp.response);
-    }
+    if (resp.response)
+      response.push(resp.response);
 
     return response;
   }
